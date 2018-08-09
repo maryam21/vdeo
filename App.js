81560165a4d40ce02a216d6f33d9dfd23379fdb7
mariam <mariam2ac1@gmail.com>
2018-08-09 10:20:56 +0100

    Add home component

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Home from './app/views/Home.js';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Home />
+    );
+  }
+}
